daf67e264426247a5a2b4dabb0e57e480603692c
e1ixyz <elimcgehee2001@gmail.com>
2024-02-04 21:23:05 -0500

    fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# code-club
-# code-club
--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// Preloader
-document.addEventListener("DOMContentLoaded", function() {
-  const preloader = document.getElementById("preloader");
-  const mainContent = document.getElementById("main-content");
-  setTimeout(function() {
-    preloader.style.display = "none";
-    mainContent.classList.add("visible");
-  }, 2900);
-});
