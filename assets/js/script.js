f539777336ef15b852475a2cfa0254e4b6c83d43
e1ixyz <elimcgehee2001@gmail.com>
2024-02-04 21:23:11 -0500

    fix

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Preloader
+document.addEventListener("DOMContentLoaded", function() {
+  const preloader = document.getElementById("preloader");
+  const mainContent = document.getElementById("main-content");
+  setTimeout(function() {
+    preloader.style.display = "none";
+    mainContent.classList.add("visible");
+  }, 2900);
+});
